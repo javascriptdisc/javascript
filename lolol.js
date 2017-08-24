474fd6f688445e9c0acff45fef895a9372c0017a
javascriptdisc <31140348+javascriptdisc@users.noreply.github.com>
2017-08-23 21:13:08 -0400

    prime generator
    finished the program

--- lolol.js
+++ lolol.js
@@ -1,14 +1,27 @@
 'use strict'
 
 var A = [];
-A[0] = 0;
-A[1] = 0;
+A[0] = false;
+A[1] = false;
 
-for(var i=0;i<98;i++){
-    A.push(1);
+for(var i=0;i<10000;i++){
+    A.push(true);
 
 }
+for(var i = 0; i < Math.sqrt(A.length); i++){
+    if(A[i]){
+        var j = i*i;
+        while(j<A.length){
+            A[j] = false;
+            j+=i;
+        }
+    }
+}
+for(var i=0;i<A.length;i++){
+    if(A[i]){
+        console.log(i);
+    }
 
-console.log(A);
+}
 
 
